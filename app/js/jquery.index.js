690f9f435e6c1bda13ed629e117216186b1265d5
alexserga <snike1985@gmail.com>
2017-10-02 17:41:04 +0300

    ui page has done

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -1,3 +1,74 @@
 ( function(){
 
+    $(function(){
+
+        $('.menu').each( function() {
+            new Menu( $(this) );
+        } );
+
+    });
+
+    var Menu = function(obj) {
+
+        //private properties
+        var _obj = obj,
+            _btn = $( '.menu-btn' ),
+            _scrollConteiner = $( 'html' );
+
+        //private methods
+        var _addEvents = function() {
+
+                _btn.on({
+                    'click': function() {
+
+                        if ( !_obj.hasClass( 'open' ) ) {
+                            _btn.addClass('active');
+                            _showMenu();
+                        } else {
+                            _btn.removeClass('active');
+                            _hideMenu();
+                        }
+                    }
+                });
+
+            },
+            _getScrollWidth = function (){
+                var scrollDiv = document.createElement( 'div'),
+                    scrollBarWidth;
+
+                scrollDiv.className = 'scrollbar-measure';
+
+                document.body.appendChild( scrollDiv );
+
+                scrollBarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+
+                document.body.removeChild(scrollDiv);
+
+                return scrollBarWidth;
+            },
+            _showMenu = function() {
+                _obj.addClass( 'open' );
+                _scrollConteiner.css( {
+                    overflowY: 'hidden',
+                    paddingRight: _getScrollWidth()
+                } );
+            },
+            _hideMenu = function() {
+                _obj.removeClass( 'open' );
+                _scrollConteiner.css( {
+                    overflowY: 'auto',
+                    paddingRight: 0
+                } );
+            },
+            _init = function() {
+                _addEvents();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
+
 } )();
